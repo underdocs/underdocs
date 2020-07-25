725fbcf2d6e154d4f2c6f10897a081a2c3259a3a
Antal Asztrik <antal.asztrik@gmail.com>
2020-07-26 01:17:05 +0200

    feat(errorprettyprinter): implement basic error printing

--- underdocs-error/src/main/kotlin/underdocs/error/pretty/DefaultErrorPrettyPrinter.kt
+++ underdocs-error/src/main/kotlin/underdocs/error/pretty/DefaultErrorPrettyPrinter.kt
@@ -1,7 +1,40 @@
 package underdocs.error.pretty
 
+import underdocs.error.BaseUnderdocsError
+
 class DefaultErrorPrettyPrinter : ErrorPrettyPrinter {
     override fun print(exception: Exception): String {
-        TODO("Not yet implemented")
+        var prettyPrintedError = """"""
+
+        if (exception is BaseUnderdocsError) {
+            prettyPrintedError = """
+                |${exception.title.toUpperCase()} - Error Code ${exception.code}
+                |Details
+                |${exception.getDescription()}
+                |
+                |Possible solution
+                |${exception.getSolution()}
+            """.trimMargin()
+
+            if (exception.cause != null) prettyPrintedError += "\nError cause:\n${exception.cause?.message}"
+        } else {
+            var errorStackTrace = ""
+
+            for(stackTraceElement in exception.stackTrace) {
+                errorStackTrace += "\t$stackTraceElement\n"
+            }
+
+            errorStackTrace = errorStackTrace.trimEnd()
+
+            prettyPrintedError = """
+                |Unexpected exception: ${exception.javaClass.name}
+                |${exception.message}
+                |
+                |Stacktrace:
+                |$errorStackTrace
+            """.trimMargin()
+        }
+
+        return prettyPrintedError
     }
 }
