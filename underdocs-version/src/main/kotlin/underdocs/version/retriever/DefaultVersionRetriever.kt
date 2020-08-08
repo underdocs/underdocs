7173a93bd768ad6dcc857d58435f80ba6bca107d
Antal Asztrik <antal.asztrik@gmail.com>
2020-08-09 00:11:45 +0200

    feat(versionretriever): add lazy delegate to reading properties

--- underdocs-version/src/main/kotlin/underdocs/version/retriever/DefaultVersionRetriever.kt
+++ underdocs-version/src/main/kotlin/underdocs/version/retriever/DefaultVersionRetriever.kt
@@ -2,11 +2,14 @@ package underdocs.version.retriever
 
 import underdocs.version.Version
 import underdocs.version.reader.VersionPropertiesReader
+import java.util.*
 
 class DefaultVersionRetriever: VersionRetriever {
     override fun get(): Version {
-        val propertiesReader = VersionPropertiesReader()
-        val versionProperties = propertiesReader.readProperties()
+        val versionProperties : Properties by lazy {
+            val propertiesReader = VersionPropertiesReader()
+            propertiesReader.readProperties()
+        }
 
         val projectVersion = versionProperties.getProperty("project.version")
         val gitCommitHash = versionProperties.getProperty("git.commit.hash")
